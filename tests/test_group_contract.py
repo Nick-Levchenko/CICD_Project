cb75a64ba283b6155b277ddc990ebf590b891ead
Николай <i@nik-levchenko.ru>
2025-02-24 13:08:41 +0700

    update

--- tests/test_group_contract.py
+++ tests/test_group_contract.py
@@ -8,9 +8,9 @@ faker = Faker()
 
 class TestGroupContract:
 
-    def test_create_group_anonym(self, university_api_utils_anonym):
-        group_helper = GroupHelper(university_api_utils_anonym)
+    def test_create_group_anonym(self, university_api_utils_admin):
+        group_helper = GroupHelper(university_api_utils_admin)
         response = group_helper.post_group({"name": faker.name()})
-        assert response.status_code == requests.status_codes.codes.unauthorized, \
+        assert response.status_code == requests.status_codes.codes.created, \
             (f"Wrong status code: {response.status_code},"
              f" expected {requests.status_codes.codes.unauthorized}")
